07fad87b157e329350647a017286f860129b74a9
caesarals <slac_@hotmail.com>
2023-06-04 13:00:34 -0400

    faker-seeds ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.each do
+    name = Faker::Movie
+    synopsis = Faker::Quotes::Shakespeare
+    director = Faker::Name
+    Movie.create(name: name, synopsis: synopsis, director: director )
+end
